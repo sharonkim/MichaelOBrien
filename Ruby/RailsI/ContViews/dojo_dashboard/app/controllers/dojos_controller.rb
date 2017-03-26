cafdbbfd5666e9c27c8d166137d5707074660d54
michaelAmused <mobrien601@gmail.com>
2017-03-26 10:33:45 -0700

    updates

--- Ruby/RailsI/ContViews/dojo_dashboard/app/controllers/dojos_controller.rb
+++ Ruby/RailsI/ContViews/dojo_dashboard/app/controllers/dojos_controller.rb
@@ -17,6 +17,29 @@ class DojosController < ApplicationController
         end
     end
 
+    def show
+        @dojo = Dojo.find(params[:id])
+    end
+
+    def edit
+        @dojo = Dojo.find(params[:id])
+    end
+
+    def update
+        @dojo = Dojo.find(params[:id])
+        if @dojo.update(dojo_params)
+            redirect_to root_path, notice: "You have successfully updated a Dojo!"
+        else
+            flash[:errors] = @dojo.errors.full_messages
+            redirect_to :back
+        end
+    end
+
+    def destroy
+        Dojo.find(params[:id]).destroy
+        redirect_to :root
+    end
+
     private
         def dojo_params
             params.require(:dojo).permit(:branch, :street, :city, :state)

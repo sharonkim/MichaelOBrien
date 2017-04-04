fe32b6bdd73e6f8af96dfbc3de9e214f37a500b8
michaelAmused <mobrien601@gmail.com>
2017-04-04 15:49:05 -0700

    updates

---
+++ Ruby/RailsII/DojoSecrets/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    has_secure_password
+    
+    EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+
+    before_save :email_lowercase
+
+    def email_lowercase
+        email.downcase!
+    end
+end

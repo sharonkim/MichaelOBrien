a320274c7311e45781b1ab1f0b1d82ce304bc26b
michaelAmused <mobrien601@gmail.com>
2017-04-06 13:50:44 -0700

    TDD and more

---
+++ Ruby/TDD/Rails_TDD/the_wall/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "users#new"
+  resources :users, only: [:new]
+  resources :messages, only: [:index, :create]
+
+  post "login" => "users#login"
+  delete "logout" => "users#logout"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
